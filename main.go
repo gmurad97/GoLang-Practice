9f866f0b8c16155c0577065f93d34df9e5c8d0d0
gmurad97 <murad.dev@bk.ru>
2024-04-20 06:04:10 +0400

    routing & server running

--- main.go
+++ main.go
@@ -7,30 +7,16 @@ import (
 )
 
 func main() {
-	// Инициализируем маршрутизатор
-	mux := http.NewServeMux()
+	route := http.NewServeMux()
+	route.HandleFunc("/", homeHandler)
 
-	// Устанавливаем обработчики маршрутов
-	mux.HandleFunc("/", homeHandler)
-	mux.HandleFunc("/about", aboutHandler)
-	mux.HandleFunc("/contact", contactHandler)
-
-	// Запускаем веб-сервер
-	log.Println("Запуск веб-сервера на :8080...")
-	err := http.ListenAndServe(":8080", mux)
-	if err != nil {
-		log.Fatal(err)
+	log.Println("Run server: http://localhost:8080")
+	serverListener := http.ListenAndServe(":8080", route)
+	if serverListener != nil {
+		log.Fatal(serverListener)
 	}
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Добро пожаловать на главную страницу!")
-}
-
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Это страница о нас.")
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Свяжитесь с нами по электронной почте: example@example.com")
+	fmt.Fprintln(w, "Welcome to home page!")
 }
